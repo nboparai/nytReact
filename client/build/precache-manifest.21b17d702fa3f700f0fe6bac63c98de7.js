b36f8c95f191874ee15904065aa0e9f6dbe54c50
Navreet Boparai <navreet.boparai@yahoo.com>
2019-01-24 11:11:08 -0800

    m

---
+++ client/build/precache-manifest.21b17d702fa3f700f0fe6bac63c98de7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f73731057de336fea209",
+    "url": "/static/css/main.0ac99bda.chunk.css"
+  },
+  {
+    "revision": "f73731057de336fea209",
+    "url": "/static/js/main.f7373105.chunk.js"
+  },
+  {
+    "revision": "6d4dbea3c83cf37645d8",
+    "url": "/static/js/1.6d4dbea3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2722a356f3897e5ead02b865f3f72e76",
+    "url": "/index.html"
+  }
+];
