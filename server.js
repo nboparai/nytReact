2e6724a3cd9ab4ff747feae4b6cf2da7bb070d09
Navreet Boparai <navreet.boparai@yahoo.com>
2019-01-24 12:07:40 -0800

    m

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname , '/client/build/index.html'))
-})
+// app.get('/*', function(req, res){
+//   res.sendFile(path.join(__dirname , '/client/build/index.html'))
+// })
 // Add routes, both API and view
 app.use(routes);
 // Set up promises with mongoose
